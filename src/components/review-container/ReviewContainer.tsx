ea86f7d0feeb92042d5883c3dd232b0cafee4288
KaptainCS3 <mbiapplegryn@gmail.com>
2024-06-17 09:53:58 +0100

    feat: complete review ui

--- src/components/review-container/ReviewContainer.tsx
+++ src/components/review-container/ReviewContainer.tsx
@@ -2,10 +2,9 @@
 import React from 'react'
 import HeaderNav from '../header-nav/HeaderNav'
 import CommentContainer from '../comment-container/CommentContainer'
-
+import RatingReview from '../rating-review/RatingReview'
 
 const ReviewContainer = () => {
-    
     return (
         <section className="w-full min-h-screen bg-primary">
             <HeaderNav
@@ -16,7 +15,8 @@ const ReviewContainer = () => {
             />
             <div className="pt-12">
                 <section className="bg-white pt-8 min-h-1/2 pb-32 px-4 rounded-t-3xl">
-                   <CommentContainer />
+                    <RatingReview />
+                    <CommentContainer />
                 </section>
             </div>
         </section>
--- src/components/review-progress/ReviewProgress.tsx
+++ src/components/review-progress/ReviewProgress.tsx
@@ -1,10 +1,23 @@
+import { StarIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 
 const ReviewProgress = () => {
+    const maxStars = 5
+    const starsArray = Array.from({ length: maxStars })
     return (
         <div>
-            <div></div>
-            <div></div>
+            {starsArray.map((_, index) => (
+                <div className="flex items-center justify-between" key={index}>
+                    <div className="flex items-center gap-1">
+                        <span>{starsArray.length - index}</span>
+                        <span>
+                            <StarIcon className={`w-5 h-5 text-gray-300`} />
+                        </span>
+                    </div>
+                    <div className="w-[80%] border rounded-2xl h-3 bg-secondary"></div>
+                    <span>12</span>
+                </div>
+            ))}
         </div>
     )
 }
