2b803aad7d4f09fe6fb788a3600273a78abff5ab
KaptainCS3 <mbiapplegryn@gmail.com>
2024-09-06 17:13:47 +0100

    fix: build and push to production

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O4pBNrDSYYqEaR3FsYYqeVcPEr5wjWakoQVRtk1dF+k=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ZXynHireM/O6MtP1ke8mwD7TUv1lhpLJrfUjAOQk+Ro=\"}"
